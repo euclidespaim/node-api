c0aee4e47266d3e9b31c458b35ca2515284b4410
euclidespaim <euclidespaim@gmail.com>
2022-04-19 10:04:49 -0300

    API concluída!

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,61 +1,78 @@
 import livros from "../models/Livro.js";
 
 class LivroController {
+  static listarLivros = (req, res) => {
+    livros
+      .find()
+      .populate("autor")
+      .exec((err, livros) => {
+        res.status(200).json(livros);
+      });
+  };
 
-    static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
-            res.status(200).json(livros);
-        })
-    }
+  static listarLivroPorId = (req, res) => {
+    const id = req.params.id;
 
-    static listarLivroPorId = (req, res) => {
-        const id = req.params.id;
+    livros
+      .findById(id)
+      .populate("autor", "nome")
+      .exec((err, livros) => {
+        if (err) {
+          res
+            .status(400)
+            .send({ message: `${err.message} - Livro ${id} não encontrado!` });
+        } else {
+          res
+            .status(200)
+            .send({ message: `Livro ${id} encontrado!`, livro: livros });
+        }
+      });
+  };
 
-        livros.findById(id, (err, livros) => {
-            if (err) {
-                res.status(400).send({message: `${err.message} - Livro ${id} não encontrado!`});
-            } else {
-                res.status(200).send({message: `Livro ${id} encontrado!`, livro: livros});
-            }
-        })
-    }
+  static cadastrarLivro = (req, res) => {
+    let livro = new livros(req.body);
 
-    static cadastrarLivro = (req, res) => {
-        let livro = new livros(req.body);
+    livro.save((err, livro) => {
+      if (err) {
+        res.status(500).send({ message: `Erro ao cadastrar livro: ${err}` });
+      } else {
+        res.status(201).send(livro.toJSON());
+      }
+    });
+  };
 
-        livro.save((err, livro) => {
-            if (err) {
-                res.status(500).send({message: `Erro ao cadastrar livro: ${err}`});
-            } else {
-                res.status(201).send(livro.toJSON());
-            }
-        })
-    }
+  static atualizarLivro = (req, res) => {
+    const id = req.params.id;
 
-    static atualizarLivro = (req, res) => {
-        const id = req.params.id;
+    livros.findByIdAndUpdate(id, { $set: req.body }, (err, livro) => {
+      if (!err) {
+        res.status(200).send({ message: "Livro atualizado com sucesso!" });
+      } else {
+        res.status(500).send({ message: `Erro ao atualizar livro: ${err}` });
+      }
+    });
+  };
 
-        livros.findByIdAndUpdate(id, {$set: req.body}, (err, livro) => {
-            if (!err) {
-                res.status(200).send({message: 'Livro atualizado com sucesso!'});
-            }else{
-                res.status(500).send({message: `Erro ao atualizar livro: ${err}`});
-            }
-        })
-    }
+  static removerLivro = (req, res) => {
+    const id = req.params.id;
 
-    static removerLivro = (req, res) => {
-        const id = req.params.id;
+    livros.findByIdAndRemove(id, (err, livro) => {
+      if (!err) {
+        res.status(200).send({ message: `Livro ${id} removido com sucesso!` });
+      } else {
+        res.status(500).send({ message: `Erro ao remover livro: ${err}` });
+      }
+    });
+  }
 
-        livros.findByIdAndRemove(id, (err, livro) => {
-            if (!err) {
-                res.status(200).send({message: `Livro ${id} removido com sucesso!`});
-            }else{
-                res.status(500).send({message: `Erro ao remover livro: ${err}`});
-            }
-        })
-    }
+  static listarLivroPorEditora = (req, res) => {
+    const editora = req.query.editora;
 
+    livros
+      .find({ 'editora': editora }, {}, (err, livros) => {
+          res.status(200).send(livros);
+      })
+  }
 }
 
 export default LivroController;
